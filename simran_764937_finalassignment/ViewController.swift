b122de3182c54dfa6ccca2b671b71cf593648880
Sumandeep2111 <sumandeep21111994@gmail.com>
2020-01-24 19:50:39 -0500

    Initial Commit

---
+++ simran_764937_finalassignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simran_764937_finalassignment
+//
+//  Created by Simran Chakkal on 2020-01-24.
+//  Copyright © 2020 simran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
